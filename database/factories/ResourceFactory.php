5b56fc2bab7ef1e5d9f6eb456eea66116fa724b2
Devdutta Bain <devdutta.bain@gmail.com>
2020-09-29 13:54:52 +0000

    Initial commit from PHPSandbox

---
+++ database/factories/ResourceFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+use App\Resource;
+
+$factory->define(Resource::class, function (Faker $faker) {
+    $userIds = App\User::select('id')->get()->pluck('id')->toArray();
+    array_push($userIds,"all");
+    return [
+        'name' => $faker->name,
+        'user_id' => $faker->randomElement($userIds),
+    ];
+});
